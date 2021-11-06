29615fd3fdc255296e06fd1ee2c0e30525412e1c
NguyenNghia <19520178@gm.uit.edu.vn>
2021-11-07 06:55:32 +0700

    rewrote the classifier layer

--- model/classifier.py
+++ model/classifier.py
@@ -3,8 +3,16 @@ from torch import nn
 class Classifier(nn.Sequential):
     def __init__(self, in_features, mid_features, out_features, drop=0.0):
         super(Classifier, self).__init__()
-        self.add_module('drop1', nn.Dropout(drop))
-        self.add_module('lin1', nn.Linear(in_features, mid_features))
-        self.add_module('relu', nn.ReLU())
-        self.add_module('drop2', nn.Dropout(drop))
-        self.add_module('lin2', nn.Linear(mid_features, out_features))
+
+        self.drop1 = nn.Dropout(drop)
+        self.lin1 = nn.Linear(in_features, mid_features)
+        self.relu = nn.ReLU()
+        self.drop2 = nn.Dropout(drop)
+        self.lin2 = nn.Linear(mid_features, out_features)
+
+    def forward(self, x):
+        x = self.lin1(self.drop1(x))
+        x = self.relu(x)
+        x = self.lin2(self.drop2(x))
+
+        return x
